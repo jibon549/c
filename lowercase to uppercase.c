8f73b86a4b6f29503cd7f376c4361db3e99974a1
Jibon Ali <jibonahmed549@gmail.com>
2019-12-01 13:55:53 +0600

    add lowercase to uppercase

---
+++ lowercase to uppercase.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    char lower;
+    printf("enter any lowercase letter : ");
+    scanf("%c",&lower);
+
+    printf("the uppercase letter : %c",lower-32);
+    return 0;
+
+}
