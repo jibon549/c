2f216f4976874b801ab32261997b040fd297720e
Jibon Ali <jibonahmed549@gmail.com>
2020-02-01 23:46:40 +0600

    add if else

---
+++ if else.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    printf("Enter an integer : ");
+    scanf("%d",&a);
+
+    if(a%2==0)
+        printf("Even\n");
+    else
+        printf("odd");
+
+
+    return 0;
+}
